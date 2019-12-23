45c613cdd80a1990607ee8ab2b44a619e91ebf12
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-23 07:30:22 +0530

    initial commits

---
+++ array_argument.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "Welcome user"
+
+# to pass arguments to our script
+# we use just 1 2 or 3 ...
+
+echo $1 $2 $3
+
+# to store the arguments in an array
+# we use $@ command
+
+args=("$@")
+
+# To print those
+
+echo "argument at position 0 is: ${args[0]}"
+echo "argument at position 1 is: ${args[1]}"
+echo "argument at position 2 is: ${args[2]}"
