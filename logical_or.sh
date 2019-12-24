5a162198c376698e82363a77059777d058ed4791
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-24 22:24:25 +0530

    logical or

---
+++ logical_or.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+echo "Logical or operator"
+
+# genral syntax is
+#
+# if [statement1] || [statement2]
+# then  ...
+#
+# or
+#
+# if [statement1 -o statement2]
+# then ...
+#
+# or
+#
+# if [[statement1] || [statement2]]
+# then ...
+
+age=17
+
+if [ $age -gt 18 ] || [ $age -lt 25 ]
+then
+  echo "Do startup"
+elif [ $age -lt 18 -o $age -gt 10 ]
+then
+  echo "Too young for startup"
+else
+  echo "Too old for startup"
+fi
