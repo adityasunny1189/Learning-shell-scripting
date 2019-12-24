874125b0b7077efb4b880385d55d41af7e742b5b
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-24 21:30:56 +0530

    file test operator

--- file_test_operator.sh
+++ file_test_operator.sh
@@ -8,7 +8,26 @@ echo "This is file test operator"
 
 echo -e "Enter filename: \c"
 read filename
-if [ -e $filename ]
+
+# common flags used in file operator :-
+# 1.)-e flag is used to check weather the
+#     file is present or not
+# 2.)-f flag is used to check if it is
+#     a regular file or not
+# 3.)-d flag is used for in search of directory
+#
+# files are of two types :-
+# 1.)character specific file. eg: text files
+# 2.)block specific files. eg: binary files
+#
+# 4.)-c flag is used for character specific files
+# 5.)-b flag is used for block specific files
+# 6.)-s flag is used for empty files
+# 7.)-r flag is used to check for read permission
+# 8.)-w flag is used for checking write permission
+# 9.)-x flag is used for checking execute permission
+
+if [ -d $filename ]
 then
   echo "File Found"
 else
